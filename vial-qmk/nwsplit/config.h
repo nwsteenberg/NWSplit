52243ce1c04a4a1b6afa53d7ca889c3d535e39fe
nwsteenberg <nikolajws@protonmail.com>
2026-09-02 20:09:46 +0200

    Added pointing device and fixed up stuff

--- vial-qmk/nwsplit/config.h
+++ vial-qmk/nwsplit/config.h
@@ -3,13 +3,20 @@
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
 
-#define SERIAL_USART_DRIVER SD1
-#define SERIAL_USART_TX_PIN GP0
-#define SERIAL_USART_RX_PIN GP0
-
 #define WS2812_PIO_IRQ_PRIORITY 0
 #define RGB_MATRIX_SPLIT_SEND_DELAY 5
 #define RGB_MATRIX_MAXIMUM_SPEED 100
 #define RGB_MATRIX_KEYPRESSES
 #define RGBLIGHT_SLEEP
 #define RGBLIGHT_SPLIT
+
+// If the sensor is wired to the RIGHT half:
+#define POINTING_DEVICE_RIGHT
+// RP2040 Hardware SPI Configuration
+#define SPI_DRIVER SPID0          // Use SPID0 for SPI0, or SPID1 for SPI1
+#define SPI_SCK_PIN GP4          // Clock pin (SCLK)
+#define SPI_MOSI_PIN GP5         // Master Out Slave In (MOSI)
+#define SPI_MISO_PIN GP6         // Master In Slave Out (MISO)
+
+// PMW3360 Specific Configuration
+#define PMW3360_CS_PIN GP7       // Chip Select (CS/NCS) pin
--- vial-qmk/nwsplit/keymaps/vial/keymap.c
+++ vial-qmk/nwsplit/keymaps/vial/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_BSLS,  
                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                          KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS, 
                                             KC_TRNS, KC_TRNS, KC_TRNS,      KC_INS,    KC_DEL,  KC_TRNS
-    )
+    ),
     [_POINTER] = LAYOUT(
                  KC_BTN3,                                                                                                  KC_TRNS, 
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  

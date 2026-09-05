752cb3c77e4b77c502362388238b57bee188e9a3
nwsteenberg <nikolajws@protonmail.com>
2026-09-05 09:16:01 +0200

    Fixed pin references in pin-header 6 pin

--- footprints/pin_header_6pin.js
+++ footprints/pin_header_6pin.js
@@ -31,12 +31,12 @@ fp.push(`(duplicate_pad_numbers_are_jumpers no)`);
 fp.push(`(embedded_fonts no)`);
 
 // Pads
-fp.push(`(pad "1" thru_hole circle (at 0 ${flipN(flip, -6.35)} ${flipR(flip, p.r + 0)}) (size 1.7 1.7) (drill 1) (layers "*.Cu" "*.Mask") (remove_unused_layers no) (thermal_bridge_angle 90)  ${p.GND})`);
-fp.push(`(pad "2" thru_hole rect (at 0 ${flipN(flip, -3.81)} ${flipR(flip, p.r + 0)}) (size 1.7 1.7) (drill 1) (layers "*.Cu" "*.Mask") (remove_unused_layers no) (thermal_bridge_angle 45)  ${p.VCC})`);
-fp.push(`(pad "3" thru_hole circle (at 0 ${flipN(flip, -1.27)} ${flipR(flip, p.r + 0)}) (size 1.7 1.7) (drill 1) (layers "*.Cu" "*.Mask") (remove_unused_layers no)  ${p.CC1})`);
-fp.push(`(pad "4" thru_hole circle (at 0 ${flipN(flip, 1.27)} ${flipR(flip, p.r + 0)}) (size 1.7 1.7) (drill 1) (layers "*.Cu" "*.Mask") (remove_unused_layers no)  ${p.CC2})`);
-fp.push(`(pad "5" thru_hole circle (at 0 ${flipN(flip, 3.81)} ${flipR(flip, p.r + 0)}) (size 1.7 1.7) (drill 1) (layers "*.Cu" "*.Mask") (remove_unused_layers no)  ${p.VCC})`);
-fp.push(`(pad "6" thru_hole circle (at 0 ${flipN(flip, 6.35)} ${flipR(flip, p.r + 0)}) (size 1.7 1.7) (drill 1) (layers "*.Cu" "*.Mask") (remove_unused_layers no)  ${p.GND})`);
+fp.push(`(pad "1" thru_hole circle (at 0 ${flipN(flip, -6.35)} ${flipR(flip, p.r + 0)}) (size 1.7 1.7) (drill 1) (layers "*.Cu" "*.Mask") (remove_unused_layers no) (thermal_bridge_angle 90)  ${p.P1})`);
+fp.push(`(pad "2" thru_hole rect (at 0 ${flipN(flip, -3.81)} ${flipR(flip, p.r + 0)}) (size 1.7 1.7) (drill 1) (layers "*.Cu" "*.Mask") (remove_unused_layers no) (thermal_bridge_angle 45)  ${p.P2})`);
+fp.push(`(pad "3" thru_hole circle (at 0 ${flipN(flip, -1.27)} ${flipR(flip, p.r + 0)}) (size 1.7 1.7) (drill 1) (layers "*.Cu" "*.Mask") (remove_unused_layers no)  ${p.P3})`);
+fp.push(`(pad "4" thru_hole circle (at 0 ${flipN(flip, 1.27)} ${flipR(flip, p.r + 0)}) (size 1.7 1.7) (drill 1) (layers "*.Cu" "*.Mask") (remove_unused_layers no)  ${p.P4})`);
+fp.push(`(pad "5" thru_hole circle (at 0 ${flipN(flip, 3.81)} ${flipR(flip, p.r + 0)}) (size 1.7 1.7) (drill 1) (layers "*.Cu" "*.Mask") (remove_unused_layers no)  ${p.P5})`);
+fp.push(`(pad "6" thru_hole circle (at 0 ${flipN(flip, 6.35)} ${flipR(flip, p.r + 0)}) (size 1.7 1.7) (drill 1) (layers "*.Cu" "*.Mask") (remove_unused_layers no)  ${p.P6})`);
 
 // Drawings on F.Fab
 fp.push(`(fp_line (start -1.27 ${flipN(flip, -6.985)}) (end -0.635 ${flipN(flip, -7.62)}) (stroke (width 0.1) (type solid)) (layer "${(flip ? "B.Fab" : "F.Fab")}") )`);

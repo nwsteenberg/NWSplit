4e7d2751109831edaa34f1a1ea60f39c0ca1303b
nwsteenberg <nikolajws@protonmail.com>
2026-09-21 08:01:25 +0200

    Removed custom SPI stuff

--- vial-qmk/nwsplit/config.h
+++ vial-qmk/nwsplit/config.h
@@ -17,11 +17,10 @@
 
 // Pointing Device & SPI Configuration (PIO SPI for GP4/GP5/GP6)
 #define POINTING_DEVICE_RIGHT
-#define SPLIT_POINTING_ENABLE
 #define SPI_SCK_PIN GP6          // Clock
-#define SPI_MOSI_PIN GP5         // MOSI (as wired on your board)
+#define SPI_MOSI_PIN GP3         // MOSI (as wired on your board)
 #define SPI_MISO_PIN GP4         // MISO
 
 // PMW3360 Specific Configuration
-#define PMW33XX_CS_PIN GP3       
+#define PMW33XX_CS_PIN GP5       
 #define ROTATIONAL_TRANSFORM_ANGLE -95
